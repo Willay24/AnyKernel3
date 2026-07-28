5ff804650a8c63672d7c1b8212e2275f5276f476
Willay <williamhmar24@gmail.com>
2026-07-29 03:41:28 +0530

    Update AnyKernel properties and metadata

--- anykernel.sh
+++ anykernel.sh
@@ -4,6 +4,10 @@
 # begin properties
 properties() { '
 kernel.string=Spacewar by @Biased69-KittenLover
+kernel.revision=5.4
+kernel.made=Biased69-Kitty
+anykernel3.made=osm0sis @ xda-developers
+kernel.compiler=Neutron clang 23.0.0
 do.devicecheck=0
 do.modules=0
 do.systemless=1
@@ -13,14 +17,17 @@ supported.versions=11.0-17.0
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
+
 # shell variables
 block=boot;
 is_slot_device=auto;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
+
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
+
 ## AnyKernel boot install
 dump_boot;
 vbmeta_disable_verification;
