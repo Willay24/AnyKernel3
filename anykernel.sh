227e81896e4bceec4010008c42d059d16674a922
William24hmar <williamhmar24@gmail.com>
2026-08-02 16:12:51 +0000

    AK3:Clean Up

--- anykernel.sh
+++ anykernel.sh
@@ -9,72 +9,29 @@ kernel.made=Biased69-Kitty
 anykernel3.made=osm0sis @ xda-developers
 kernel.compiler=Neutron clang 24.0.0
 do.devicecheck=0
-do.modules=0
-do.systemless=1
 do.cleanup=1
-do.cleanuponabort=0
 supported.versions=11.0-17.0
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
 
-### AnyKernel install
-## boot files attributes
-boot_attributes() {
-set_perm_recursive 0 0 755 644 $RAMDISK/*;
-set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
-} # end attributes
-
-# boot shell variables
-BLOCK=auto;
+# shell variables
+block=boot;
 is_slot_device=auto;
-RAMDISK_COMPRESSION=auto;
-PATCH_VBMETA_FLAG=auto;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
 
-# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-# boot install
-dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
-
+## AnyKernel boot install
+dump_boot;
 vbmeta_disable_verification;
 
-write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+## Ensure pushed replacement files keep correct ownership/permissions
+set_perm 0 0 644 lib/modules/modules.load.recovery;
+set_perm 0 0 644 lib/modules/modules.dep;
+
+write_boot;
 ## end boot install
-
-
-## vendor_kernel_boot shell variables
-#BLOCK=vendor_kernel_boot;
-#IS_SLOT_DEVICE=auto;
-#RAMDISK_COMPRESSION=auto;
-#PATCH_VBMETA_FLAG=auto;
-
-# reset for vendor_kernel_boot patching
-#reset_ak;
-
-# vendor_kernel_boot install
-#split_boot; # skip unpack/repack ramdisk, e.g. for dtb on devices with hdr v4 and vendor_kernel_boot
-
-#flash_boot;
-## end vendor_kernel_boot install
-
-## vendor_boot files attributes
-#vendor_boot_attributes() {
-#set_perm_recursive 0 0 755 644 $RAMDISK/*;
-#set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
-#} # end attributes
-
-# vendor_boot shell variables
-#BLOCK=vendor_boot;
-#IS_SLOT_DEVICE=auto;
-#RAMDISK_COMPRESSION=auto;
-#PATCH_VBMETA_FLAG=auto;
-
-# reset for vendor_boot patching
-#reset_ak;
-
-# vendor_boot install
-#dump_boot; # use split_boot to skip ramdisk unpack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
-
-#write_boot; # use flash_boot to skip ramdisk repack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
-## end vendor_boot install
